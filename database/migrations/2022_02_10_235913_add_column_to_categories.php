7e17ad3513a4d705c3c48514c8dcef22c100979a
min khant <minkhantzaw1210@gmail.com>
2022-02-17 11:41:13 +0630

    test

--- database/migrations/2022_02_10_235913_add_column_to_categories.php
+++ database/migrations/2022_02_10_235913_add_column_to_categories.php
@@ -14,7 +14,7 @@ class AddColumnToCategories extends Migration
     public function up()
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->string('slug')->nullable()->unique()->after('title');
+//            $table->string('slug')->nullable()->unique()->after('title');
         });
     }
 
